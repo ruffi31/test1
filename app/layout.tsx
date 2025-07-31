7df71c0785790bf81aeea95b3e42bbbb5887ae5b
ruffi31 <arauf.cs@gmail.com>
2025-07-31 13:00:18 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@
 import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import type { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Sherwood Pharmacy', // Browser tab title
+  description: 'Your trusted local pharmacy',
+  icons: {
+    icon: '/logo.jpg', // Path from the public folder
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
